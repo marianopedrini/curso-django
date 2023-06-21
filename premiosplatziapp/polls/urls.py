90295de30159dcfa8df995b70166cc75da9afc64
Mariano Pedrini <pedrini.mariano96@gmail.com>
2023-06-21 16:23:35 -0300

    finished the beginners course

--- premiosplatziapp/polls/urls.py
+++ premiosplatziapp/polls/urls.py
@@ -2,9 +2,15 @@ from django.urls import path
 
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
-    path('', views.index, name="index")
+    #example: /polls/
+    path('', views.IndexView.as_view(), name="index"),
+    #example: /polls/5/ 
+    path('<int:pk>/', views.DetailView.as_view(), name="detail"),
+    #example: /polls/5/results 
+    path('<int:pk>/results', views.ResultView.as_view(), name="results"),
+    #example: /polls/5/vote
+    path('<int:question_id>/vote', views.vote, name="vote")
+    #<int:question_id> es el parámetro question_id que se le pasa a cada función dentro de views.py
 ]
-
-
-
